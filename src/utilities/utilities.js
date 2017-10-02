025bae15e58bd244cbab22241a3f857f67ffbeb3
Marko <markoljubic993@gmail.com>
2017-10-03 01:10:01 +0200

    make header presentational component, and make app responsive

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,3 +1,20 @@
 export const axios = require('axios');
 
 export const googleMapURL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA7IsXjBoZVuf_9F8Xtn0swTg_UIeckIZo&v=3.exp&libraries=geometry,drawing,places';
+
+export const apiURL = 'https://itk-exam-api.herokuapp.com/api/offices';
+
+export const headerLinks = [
+  {
+    link: '/Offices/List',
+    name: 'List',
+  },
+  {
+    link: '/Offices/Grid',
+    name: 'Grid',
+  },
+  {
+    link: '/Offices/Map',
+    name: 'Map',
+  },
+];
